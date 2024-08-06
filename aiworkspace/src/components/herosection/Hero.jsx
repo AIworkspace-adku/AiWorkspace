5d0971a63a21da508e2699e7b258c179a4c9b5fd
Ayushpani <ayushpanigrahi84@gmail.com>
2024-08-06 19:39:13 +0530

    made changes in hero section

--- aiworkspace/src/components/herosection/Hero.jsx
+++ aiworkspace/src/components/herosection/Hero.jsx
@@ -4,22 +4,28 @@ import Split from 'react-split';
 import './Hero.css';
 
 const Hero = () => {
-  return (
-    <div className="hero-container">
-      <Split className="split" sizes={[45, 55]} minSize={200} expandToMin={false} gutterSize={10} gutterAlign="center" snapOffset={30} dragInterval={1} direction="horizontal" cursor="col-resize">
-        <div className="land-left">
-          <h1>Workspace like No other</h1>
+    return (
+        <div className="hero-container">
+            <Split className="split" sizes={[45, 55]} minSize={200} expandToMin={false} gutterSize={10} gutterAlign="center" snapOffset={30} dragInterval={1} direction="horizontal" cursor="col-resize">
+                <div className="land-left">
+                    <h1>Workspace like No other</h1><br></br>
+                    <button class="button">
+                        Signup
+
+                    </button>
+
+
+                </div>
+                <div className="land-right">
+                    <Spline
+                        scene="https://prod.spline.design/ovoLpMM5FYMjDtjF/scene.splinecode"
+                        width={800}
+                        height={800}
+                    />
+                </div>
+            </Split>
         </div>
-        <div className="land-right">
-          <Spline
-            scene="https://prod.spline.design/ovoLpMM5FYMjDtjF/scene.splinecode"
-            width={1000}
-            height={1000}
-          />
-        </div>
-      </Split>
-    </div>
-  );
+    );
 };
 
 export default Hero;
