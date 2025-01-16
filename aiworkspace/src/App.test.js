4a80f1f11c7cb63fcf834d6b4bd7f0812ccac785
KrithikPatil <patilkrithik@gmail.com>
2025-01-16 09:47:27 +0530

    fixed the frontend tests

--- aiworkspace/src/App.test.js
+++ aiworkspace/src/App.test.js
@@ -1,14 +1,12 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { act } from 'react';
 
 test('renders learn react link', () => {
-	act(() => {
-		render(
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		);
-	});
+	render(
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	);
+	// You can add assertions here to check if the correct elements are rendered
 });
